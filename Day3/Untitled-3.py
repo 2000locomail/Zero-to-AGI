3bec223ac9d05bbf13bf51f4292d103acd46c522
Sarvesh Dwivedi <2000locomail@gamil.com>
2026-03-14 13:25:10 +0530

    +one question

--- Day3/Untitled-3.py
+++ Day3/Untitled-3.py
@@ -18,3 +18,14 @@ elif(marks>=70 and marks < 80):
     print("Grade : C")
 else:
     print("Grade : D")
+
+A = int(input("A : "))
+G = input("M/F : ").upper()
+if((A == 1 or A == 2) and G == "M"):
+    print("fee is 100")
+elif((A == 3 or A == 4) and G == "F"):
+    print("fee is 200")
+elif(A == 5 and G == "M"):
+    print("fee is 300")
+else:
+    print("no fee")
