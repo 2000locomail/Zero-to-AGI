0b70ab326012ab07d26c7151ee1e79c2ddacb6ed
Sarvesh Dwivedi <2000locomail@gamil.com>
2026-03-16 21:12:32 +0530

    Day 6 Learning ( Indexing, Slicing, String Functions, Lists)

--- Day6/Learning.py
+++ Day6/Learning.py
@@ -1,4 +1,4 @@
-"""# Indexing
+# Indexing
 str = "Apna College"
 ch = str[0]
 print(ch)
@@ -47,7 +47,7 @@ print(len(name))
 
 str = "The Price of My Loptop is $ 623.3"
 str = str.count("$")
-print(str)"""
+print(str)
 
 # Lists in Python
 # A built-in data type that stores set of values
@@ -57,3 +57,30 @@ print(marks)
 print(len(marks))
 print(marks[0])
 print(marks[1])
+
+# Lists are mutable
+student = ["Karan", 98.5, 17, "Delhi"]
+print(student[0])
+student[0] = "Jeax"
+print(student)
+
+# List Slicing (Similar to string slicing)
+marks = [88, 78, 90, 79, 97, 95]
+marks = marks[0:5]
+print(marks)
+
+# List Methods
+list = [2, 1, 3]
+print(list.append(4))
+print(list.sort())
+print(list.sort(reverse=True))
+print(list)
+
+list = [2, 1, 3]
+list.insert(1,5)
+list.reverse()
+print(list)
+
+list = [2, 1, 3, 4]
+list.pop(2)
+print(list)
