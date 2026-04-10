817be0c7f135ed4864882569bca8d8cc24c1e821
Sarvesh Dwivedi <2000locomail@gamil.com>
2026-04-10 21:44:29 +0530

    OOP in Python (Class is a biueprint for creating objects.)

---
+++ Day025/Learning.py
@@ -0,0 +1,17 @@
+# OOP in Python
+# To map with real world scenarios, we started using objects in code.
+# This is called object oriented programming.
+# Class & Object in Python
+# Class is a biueprint for creating objects.
+class Student:
+    name = "Karan"
+# creating objects (instance)
+s1 = Student()
+print(s1.name)
+
+class Car:
+    color = "blue"
+    model = "G 65"
+car1 = Car()
+print(car1.color)
+print(car1.model)
